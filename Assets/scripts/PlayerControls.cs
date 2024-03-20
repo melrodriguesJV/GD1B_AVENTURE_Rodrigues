2441cb7b1b91fe5542d67c8e2661d3bf07514d8c
melrodriguesJV <mel1985rod@gmail.com>
2024-03-20 15:47:59 +0100

    start move

---
+++ Assets/scripts/PlayerControls.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private bool isMoving;
+
+    private Vector2 input;
+
+    private void Update()
+    {
+        //attribue les stockages de droite/gauche et haut/bas à la variable input
+        if (!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            //vérifie si une touche est pressée avec un statut nul
+            if (input != Vector2.zero)
+            {
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                StartCoroutine(Move(targetPos));
+            }
+        }
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+        // tant que le perso bouge
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            // prend la position de départ et la modifie
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
+}
