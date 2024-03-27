1720c96f2bf5aad0f23f920a4ef5ea1510aaba46
melrodriguesJV <mel1985rod@gmail.com>
2024-03-27 14:17:40 +0100

    vertical cam

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform player;
-    [SerializeField] private float speed;
     private Vector3 velocity = Vector3.zero;
     void Update()
     {
-        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
     }
 }
