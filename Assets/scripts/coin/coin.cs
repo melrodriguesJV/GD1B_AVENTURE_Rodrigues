74167cffd87ae0a3f2acb87705e72b1521a4d505
melrodriguesJV <mel1985rod@gmail.com>
2024-04-04 18:07:36 +0200

    coin counter

--- Assets/scripts/coin/coin.cs
+++ Assets/scripts/coin/coin.cs
@@ -5,12 +5,19 @@ public class coin : MonoBehaviour
     [SerializeField] private int value;
     private bool hasTriggered;
 
+    private CoinManager coinManager;
+
+    private void Start()
+    {
+        coinManager = CoinManager.instance;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && !hasTriggered)
         {
             hasTriggered = true;
-            //give score to player
+            coinManager.ChangeCoins(value);
             Destroy(gameObject);
         }
     }
