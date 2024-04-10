469d684c77efbb031e8b3a8665791945655e8955
melrodriguesJV <mel1985rod@gmail.com>
2024-04-10 10:02:32 +0200

    iframes

--- Assets/scripts/health/Health.cs
+++ Assets/scripts/health/Health.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header ("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
+
+    [Header("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRend;
 
     private void Awake()
     {
         currentHealth = startingHealth;
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(float _damage)
@@ -19,6 +26,7 @@ public class Health : MonoBehaviour
         if(currentHealth > 0)
         {
             //player hurt
+            StartCoroutine(Invulnerabilty());
         }
         else
         {
@@ -30,5 +38,18 @@ public class Health : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E))
             TakeDamage(1);
+    }
+
+    private IEnumerator Invulnerabilty()
+    {
+        Physics2D.IgnoreLayerCollision(6, 7, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(6, 7, false);
     }
 }
