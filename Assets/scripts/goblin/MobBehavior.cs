199832f4f5674c451c42d544ed88021b22e5bf9d
melrodriguesJV <mel1985rod@gmail.com>
2024-04-10 15:27:39 +0200

    damage installement

--- Assets/scripts/goblin/MobBehavior.cs
+++ Assets/scripts/goblin/MobBehavior.cs
@@ -11,6 +11,15 @@ public class MobBehavior : MonoBehaviour
     [SerializeField] private BoxCollider2D boxCollider;
     [SerializeField] private LayerMask playerLayer;
     private float cooldownTimer = Mathf.Infinity;
+    //references
+    private Animator anim;
+    private Health playerHealth;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        //anim.SetTrigger("nom de l'animation d'attaque");
+    }
 
     private void Update()
     {
@@ -21,7 +30,7 @@ public class MobBehavior : MonoBehaviour
         {
             if (cooldownTimer >= attackCooldown)
             {
-                //attack
+                cooldownTimer = 0;
             }
         }     
     }
@@ -31,7 +40,10 @@ public class MobBehavior : MonoBehaviour
         RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, 
             new Vector3(boxCollider.bounds.size.x *range, boxCollider.bounds.size.y, boxCollider.bounds.size.z), 
             0, Vector2.left, 0, playerLayer);
-       
+
+        if (hit.collider != null)
+            playerHealth = hit.transform.GetComponent<Health>();
+        
         return hit.collider != null;
     }
 
@@ -40,5 +52,12 @@ public class MobBehavior : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
             new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
+    }
+
+    private void DamagePlayer()
+    {
+        //if player is still in range damage him
+        if (PlayerInSight())
+            playerHealth.TakeDamage(damage);
     }
 }
