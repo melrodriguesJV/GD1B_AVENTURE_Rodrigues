04ff52f2897f7f323e38b9d63cbca2dfc2554eb7
melrodriguesJV <mel1985rod@gmail.com>
2024-04-10 14:52:48 +0200

    adjustable range

--- Assets/scripts/goblin/MobBehavior.cs
+++ Assets/scripts/goblin/MobBehavior.cs
@@ -6,6 +6,7 @@ public class MobBehavior : MonoBehaviour
 {
     [SerializeField] private float attackCooldown;
     [SerializeField] private float range;
+    [SerializeField] private float colliderDistance;
     [SerializeField] private int damage;
     [SerializeField] private BoxCollider2D boxCollider;
     [SerializeField] private LayerMask playerLayer;
@@ -27,8 +28,9 @@ public class MobBehavior : MonoBehaviour
 
     private bool PlayerInSight()
     {
-        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x, 
-            boxCollider.bounds.size, 0, Vector2.left, 0, playerLayer);
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, 
+            new Vector3(boxCollider.bounds.size.x *range, boxCollider.bounds.size.y, boxCollider.bounds.size.z), 
+            0, Vector2.left, 0, playerLayer);
        
         return hit.collider != null;
     }
@@ -36,6 +38,7 @@ public class MobBehavior : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x, boxCollider.bounds.size);
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
     }
 }
