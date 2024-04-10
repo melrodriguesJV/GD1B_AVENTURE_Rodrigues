a62ab3db3c98dcf173fff7216bbbf788c8edbf9e
melrodriguesJV <mel1985rod@gmail.com>
2024-04-10 10:31:09 +0200

    start gob behavior

--- Assets/scripts/goblin/MobBehavior.cs
+++ Assets/scripts/goblin/MobBehavior.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class MobBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private int damage;
+    private float cooldownTimer = Mathf.Infinity;
+
+    private void Update()
     {
-        
+        cooldownTimer += Time.deltaTime;
+
+        //attck only when player in sight ?
+        if (PlayerInSight())
+        {
+
+        }
+        if (cooldownTimer >= attackCooldown)
+        {
+            //attack
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private bool PlayerInSight()
     {
-        
+        return false;
     }
 }
