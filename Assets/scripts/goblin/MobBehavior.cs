94400e6bed49e5e2e325039179a281c0917fc46e
melrodriguesJV <mel1985rod@gmail.com>
2024-04-10 14:06:09 +0200

    player detection

--- Assets/scripts/goblin/MobBehavior.cs
+++ Assets/scripts/goblin/MobBehavior.cs
@@ -6,6 +6,8 @@ public class MobBehavior : MonoBehaviour
 {
     [SerializeField] private float attackCooldown;
     [SerializeField] private int damage;
+    [SerializeField] private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask playerLayer;
     private float cooldownTimer = Mathf.Infinity;
 
     private void Update()
@@ -15,16 +17,24 @@ public class MobBehavior : MonoBehaviour
         //attck only when player in sight ?
         if (PlayerInSight())
         {
-
-        }
-        if (cooldownTimer >= attackCooldown)
-        {
-            //attack
-        }
+            if (cooldownTimer >= attackCooldown)
+            {
+                //attack
+            }
+        }     
     }
 
     private bool PlayerInSight()
     {
-        return false;
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 
+            0, Vector2.left, 0, playerLayer);
+       
+        return hit.collider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(boxCollider.bounds.center, boxCollider.bounds.size);
     }
 }
