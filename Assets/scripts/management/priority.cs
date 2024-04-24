750316e3c3460ce5e39fa67f7f0a4428970f4ef7
melrodriguesJV <mel1985rod@gmail.com>
2024-04-24 10:27:55 +0200

    attempt load title

---
+++ Assets/scripts/management/priority.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class priority : MonoBehaviour
+{
+    void Start()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+       
+          
+    }
+}
